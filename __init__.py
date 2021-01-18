e34dd16d5c094bae68812231fee66ecc02498795
Karl Roe <karl.roe@ucdconnect.ie>
2021-01-18 18:23:28 +0000

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-import platform
-
-def joke():
-    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-            u'Beiherhund das Oder die Flipperwaldt gersput.')
-
-print(joke())
-print(platform.machine())
