3c15bb63a17a784ba59d5c5fffa1ae14c71a1eed
Karl Roe <karl.roe@ucdconnect.ie>
2021-01-18 18:22:33 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='SE',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='',
+      author='Flying Circus',
+      author_email='',
+      license='MIT',
+      packages=['SE'],
+      install_requires=[
+          'sysinfo',
+      ],
+      zip_safe=False)
